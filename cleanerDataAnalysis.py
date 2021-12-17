798af86dac99530dabae84e11009a63d913500bc
Ryan Mizukami <rmizukami741@gmail.com>
2021-12-16 18:45:12 -0800

    added a plot but commentted it out

--- cleanerDataAnalysis.py
+++ cleanerDataAnalysis.py
@@ -148,6 +148,18 @@ print('SB Barrier = ', meanphi, ' +/- ', dmeanphi)
 # del_fit_b=math.sqrt(cov[0][0])
 # del_fit_m=math.sqrt(cov[1][1])
 # plt.plot(fit_x_span,linFunc(fit_x_span,fit_b,fit_m),'r')
+# p6 = plt.figure(6,figsize=(12,9))
+# plt.xlabel('$V_s$ [V]', fontsize = 12)
+# plt.ylabel('I [mA]', fontsize = 12)
+# plt.title('PPMS Data At Various Temperatures', fontsize = 12)
+# plt.plot(ppmsr[0][:], ppmsr[1][:], 'r', label='300 K')
+# plt.plot(ppms285[0][:], ppms285[1][:], 'c', label='285 K')
+# plt.plot(ppms270[0][:], ppms270[1][:], 'g', label='270 K')
+# plt.plot(ppms255[0][:], ppms255[1][:], 'y', label='255 K')
+# plt.plot(ppms240[0][:], ppms240[1][:], 'm', label='240 K')
+# plt.grid(True,which='both')
+# plt.legend()
+
 
 
 # print(phi1)
